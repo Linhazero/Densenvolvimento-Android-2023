efad3a7520db0f2689ebc91cd8a5c4e9368a1109
Victor Rodrigues <victorxdrodrigues@gmail.com>
2023-05-09 14:19:58 -0300

    Uso dos metodos get e set.

--- AppListaCurso/app/src/main/java/devandroid/victor/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/victor/applistacurso/view/MainActivity.java
@@ -10,6 +10,10 @@ import devandroid.victor.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        //Atribuir dados e valores ao objeto
+        //Comforme o seu modelo
+        pessoa.setPrimeiroNome("Victor");
+        pessoa.setSobreNome("Rodrigues");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("98-984320139");
+
+        outraPessoa = new Pessoa();
+        //Atribuir dados e valores ao objeto
+        //Comforme o seu modelo
+        outraPessoa.setPrimeiroNome("Oliver");
+        outraPessoa.setSobreNome("Rodrigues");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("98-984320235");
+
+        dadosPessoa = "Primeiro Nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro Nome: ";
+        dadosOutraPessoa += pessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += pessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += pessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de contato: ";
+        dadosOutraPessoa += pessoa.getTelefoneContato();
+
+        int parada = 0;
     }
 }
