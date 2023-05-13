2b9a89e8fdd2d5514d2a02a5d05de925053ae0cd
Victor Rodrigues <victorxdrodrigues@gmail.com>
2023-05-13 10:04:41 -0300

    Controller MVC btn Salvar

--- AppListaCurso/app/src/main/java/devandroid/victor/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/victor/applistacurso/controller/PessoaController.java
@@ -1,4 +1,23 @@
 package devandroid.victor.applistacurso.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.victor.applistacurso.model.Pessoa;
+
 public class PessoaController {
+
+    @NonNull
+    @Override
+    public String toString() {
+
+        Log.d("MVC_Controller","Controller iniciada");
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+        Log.d("MVC_Controller","Controller Salvo: "+pessoa.toString());
+
+    }
 }
